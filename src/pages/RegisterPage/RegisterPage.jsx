57543585a0c525dfda3f65db17a221f837c50cad
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-05-08 22:35:39 +0300

    register form

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,29 @@
+import { useDispatch } from "react-redux";
 import { RegisterForm, RegisterFormInput, RegisterFormLabel,RegisterFormBtn } from "./RegisterPage.styled";
+import { register } from "redux/authSlice/operations";
 
 const RegisterPage = () => {
+    const dispatch = useDispatch();
+    console.log(111);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+
+        dispatch(
+            register({
+                name: form.elements.name.value,
+                email: form.elements.value,
+                password: form.elements.password.value
+            })
+        )
+        form.reset();
+    };
+
     return (
         <>
             <h1>Register</h1>
-            <RegisterForm>
+            <RegisterForm onSubmit={handleSubmit}>
                 <RegisterFormLabel>
                     User Name
                     <RegisterFormInput type="text" />
@@ -19,7 +38,7 @@ const RegisterPage = () => {
                 </RegisterFormLabel> 
                 
             </RegisterForm>
-            <RegisterFormBtn>OK</RegisterFormBtn>
+            <RegisterFormBtn type="submit">Register</RegisterFormBtn>
             
         </>
     )
